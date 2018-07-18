b6b7a3bc821b85b5e198c47689b3fb33adc5ff22
Anastasija Pischik <nastijpischik@gmail.com>
2018-07-18 10:50:30 +0300

    update

--- Empoyee.cs
+++ Empoyee.cs
@@ -4,9 +4,29 @@ using System.Text;
 
 namespace Task3
 {
-    public class Employee
+    class Employee  : IComparable <Employee>, IComparer<Employee>
     {
         public string FullName { set; get; }
         public int Salary { set; get; }
+
+        public int CompareTo(Employee e1)
+        {
+            if (this.Salary < e1.Salary)
+                   return -1;
+            else if (this.Salary > e1.Salary)
+                   return 1;
+            else
+                   return 0;
+        }
+
+        public int Compare(Employee e1, Employee e2)
+        {
+            if (String.Compare(e1.FullName, e2.FullName) < 0)
+                return -1;
+            else if (String.Compare(e1.FullName, e2.FullName) > 0)
+                return 1;
+            else
+                return 0;
+        }       
     }
 }
