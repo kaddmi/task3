f0efb502084c1c0f3f9636e36a117955064df567
Anastasija Pischik <nastijpischik@gmail.com>
2018-07-18 13:21:56 +0300

    upd

--- Program.cs
+++ Program.cs
@@ -8,16 +8,12 @@ namespace Task3
         static void Main(string[] args)
         {
             List<Employee> employees = new List<Employee>(3);
-            employees.Add(new Employee() { FullName = "Pischik A.", Salary = 400});
-            employees.Add(new Employee() { FullName = "Ivanova J.", Salary = 200});
-            employees.Add(new Employee() { FullName = "Komolova K.", Salary = 500 });
-            
+            employees.Add(new Employee() { FullName = "Pischik A.", Salary = 400} );
+            employees.Add(new Employee() { FullName = "Ivanova J.", Salary = 200} );
+            employees.Add(new Employee() { FullName = "Komolova K.", Salary = 500} ); 
             employees.Sort();
-            for (int i = 0; i < employees.Count; i++)
-            {
-                Employee emp = employees[i];
+            foreach(Employee emp in employees)
                 Console.WriteLine("Full name: {0}; Salary: {1}", emp.FullName, emp.Salary);
-            }
             Console.Read();
         }
     }
